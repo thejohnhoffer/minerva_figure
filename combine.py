ba7563d891093f90b9021aa960fa859101dd9fa0
John Hoffer <john_hoffer@hms.harvard.edu>
2018-04-09 17:11:53 -0400

    working blending

--- combine.py
+++ combine.py
@@ -1,4 +1,5 @@
 import numpy as np
+import glob, re
 import cv2
 import os
 
@@ -18,42 +19,136 @@ def load_file(fmt, args):
         return cv2.imread(img_name, 0)
     return None
 
-# Load all the image files in a range
-def load_files(fmt, all_vars, n_const=0):
-    """ Load a bunch of files
+# Find all the file paths in a range
+def find_files(fmt):
+    """ Parse directory structure
 
     Args:
         fmt: string defining file pattern
-        all_vars: tuple of variables for pattern
-        n_const: number of constant variables
+
+    Returns:
+        size in channels, times, LOD, Z, Y, X
+        image tile size in pixels: y, x
     """
+    num_dim = 6
+    voxels = np.uint16([1, 0, 0])
+    sizes = np.zeros(num_dim, dtype=np.uint16)
+    all_iglob = fmt.format(*(('*',) * num_dim))
+    all_regex = fmt.format(*(('(\d+)',) * num_dim))
 
-    if n_const == len(all_vars):
-        yield load_file(fmt, all_vars)
-    else:
-        n_new = len(all_vars) - n_const
-        temp_vars = list(all_vars[:n_const] + (0,)*n_new)
-        while True:
-            temp_vars[-n_new] += 1
-            next_vars = tuple(temp_vars)
-            next_file = load_files(fmt, next_vars, n_const-1)
-            if next_file is not None:
-                yield next_data
-                break
-            return None
+    # Find all files matching the pattern
+    for name in glob.iglob(all_iglob):
+        # Extract parameters for each dimension
+        match = next(re.finditer(all_regex, name), None)
+        if match is not None:
+            coords = match.group(*range(1,num_dim+1))
+            # Take the maximum of all coordinates
+            sizes = np.maximum(sizes, np.uint16(coords))
+            # Read first image
+            if not all(voxels):
+                file_name = match.group(0)
+                file_data = cv2.imread(file_name, 0)
+                voxels[-2::] = file_data.shape
 
+    # zero-based sizes
+    return sizes + 1, voxels[-2::]
+
+def blend_tile(t, l, z, y, x, **kwargs):
+    """
+    Keywords:
+        shape: uint16 2vec y,x pixel shape of one tile
+        ranges: float32 2 by N-channel min,max range
+        colors: float32 2 by N-channel b,g,r max color
+
+    Returns:
+        uint8 y by x by 3 color BGR image
+    """
+    all_bgr = kwargs.get('colors', np.float32([(1.0, 1.0, 1.0)]))
+    all_thresh = kwargs.get('ranges', np.float32([(0.0, 1.0)]))
+    tile_shape = kwargs.get('shape', np.uint16([1024, 1024]))
+    color_tile_shape = tuple(tile_shape) + (3,)
+
+    # numpy buffer for interpolation
+    img_buffer = np.zeros(color_tile_shape, dtype=np.float32)
+
+    # Process all channels
+    for c in range(n_channel):
+        # Fraction of full color
+        channel_bgr = all_bgr[c % len(all_bgr)]
+        max_color = channel_bgr / float(n_channel)
+        # Fraction of full range
+        channel_thresh = all_thresh[c % len(all_bgr)]
+        lowest, highest = channel_thresh
+        clip_size = highest - lowest
+        # Load the image file
+        params = c, TIME, LOD, z, y, x
+        img_data = load_file(in_full_format, params)
+        img_data = np.float32(img_data) / 255.0
+        # Clip the image
+        clip_data = np.clip(img_data, *channel_thresh)
+        norm_data = (clip_data - lowest) / clip_size
+        # Add the colored data to the image
+        y_shape, x_shape = norm_data.shape
+        gray_bgr_data = cv2.cvtColor(norm_data, cv2.COLOR_GRAY2RGB)
+        img_buffer[0:y_shape, 0:x_shape] += gray_bgr_data * channel_bgr
+
+    return np.uint8(img_buffer*255)
 
 if __name__ == "__main__":
 
     # Constants
     IN_DIR = "/media/john/420D-AC8E/cycif_images/40BP_59/tiles/"
-    IN_NAME_FORMAT = "C{:d}-T{:d}-Z{:d}-L{:d}-Y{:d}-X{:d}.png"
-    IN_NAME_NUM_VARS = 6
-    
-    # Full path format of input files
-    full_format = os.path.join(IN_DIR, IN_NAME_FORMAT)
-    all_variables = (0,) * IN_NAME_NUM_VARS
+    IN_NAME_FORMAT = "C{0:}-T{1:}-Z{3:}-L{2:}-Y{4:}-X{5:}.png"
 
-    # Load all input files
-    for fi in iter(load_files(full_format, all_variables)):
-        print(img)
+    # Test parameters
+    NOW = "2018_04_09/5PM"
+    OUT_DIR = "/home/john/2018/data/cycif_out/{}/40BP_59/tiles/".format(NOW)
+    OUT_NAME_FORMAT = "T{0:}-Z{2:}-L{1:}-Y{3:}-X{4:}.png"
+
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+
+    # Full path format of input files
+    in_full_format = os.path.join(IN_DIR, IN_NAME_FORMAT)
+    out_full_format = os.path.join(OUT_DIR, OUT_NAME_FORMAT)
+
+    # Find range of image tiles
+    ctlzyx_shape, tile_shape = find_files(in_full_format)
+    zyx_shape = ctlzyx_shape[-3::]
+    n_channel = ctlzyx_shape[0]
+
+    ALL_THRESH = np.float32([
+        [0, 1.0],
+        [0, 0.1],
+        [0, 0.05],
+        [0, 0.1],
+    ])
+    ALL_BGR = np.float32([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1],
+    ])
+    TIME = 0
+    LOD = 0
+    # Process all z,y,x tiles
+    for i in range(np.prod(zyx_shape)):
+        z,y,x = np.unravel_index(i, zyx_shape)
+
+        # DERP
+        if z != 0:
+            continue
+
+        img_buffer = blend_tile(TIME, LOD, z, y, x, **{
+            'colors': ALL_BGR,
+            'ranges': ALL_THRESH,
+            'shape': tile_shape
+        })
+
+        # Write the image buffer to a file
+        out_file = out_full_format.format(TIME, LOD, z, y, x)
+        try:
+            cv2.imwrite(out_file, img_buffer)
+        except Exception as e:
+            print (e)
+
