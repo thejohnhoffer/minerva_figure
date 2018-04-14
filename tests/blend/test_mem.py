835ed7fe6a7d94f00269aca17c41946c5b6ef21b
thejohnhoffer <hoffexi@gmail.com>
2018-04-14 01:51:05 -0400

    blue and yellow chessboard test works

--- tests/blend/test_mem.py
+++ tests/blend/test_mem.py
@@ -9,18 +9,26 @@ class TestKey(Key):
     """
     u16 = np.uint16(range(2**16))
 
-    # Sample ranges and colors
-    fro0to100 = np.float32([0, 1])
-    fro0to50 = np.float32([0, 0.5])
-    fro50to100 = np.float32([0.5, 1])
+    # Sample named colors
     white = np.float32([1, 1, 1])
+    yellow = np.float32([0, 1, 1])
     green = np.float32([0, 1, 0])
+    blue = np.float32([1, 0, 0])
+    red = np.float32([0, 0, 1])
 
     # Test input images
     u16_all = Key.square(u16)
+    u16_7x7 = Key.square(u16[:49])
+    # On/off grid
+    u16_chess0 = np.uint16(u16_7x7 % 2 == 0)*u16[-1]
+    u16_chess1 = np.uint16(u16_7x7 % 2 == 1)*u16[-1]
+    # Slightly overlapping bars
+    u16_bar0 = np.tile(Key.square(u16)[:-2:64].T, 3)
+    u16_bar1 = np.tile(Key.square(u16)[1:-1:64].T, 3)
+    u16_bar2 = np.tile(Key.square(u16)[2::64].T, 3)
     # Test intermediate images
-    u16_0to50 = Key.norm_cut(u16_all, fro0to50)
-    u16_50to100 = Key.norm_cut(u16_all, fro50to100)
+    u16_0to50 = Key.u8_cut_norm(u16_all, Key.fro(0, 50))
+    u16_50to100 = Key.u8_cut_norm(u16_all, Key.fro(50, 100))
 
     def sanity_check(tile_pair):
         """ Compare basic details of two images
@@ -101,9 +109,9 @@ def test_tile_1channel_gray():
     # START TEST
     test_id = '1channel_gray_all'
     tiles_in = TestKey.u16_all[np.newaxis]
-    tile_ok = TestKey.to_bgr(TestKey.u16_all)
+    tile_ok = TestKey.u8_colorize(TestKey.u16_all)
     test_keys = {
-        'ranges': TestKey.fro0to100[np.newaxis],
+        'ranges': TestKey.fro(0, 100)[np.newaxis],
         'colors': TestKey.white[np.newaxis],
         'shape': tiles_in[0].shape
     }
@@ -113,9 +121,9 @@ def test_tile_1channel_gray():
     # START TEST
     test_id = '1channel_gray_0to50'
     tiles_in = TestKey.u16_all[np.newaxis]
-    tile_ok = TestKey.to_bgr(TestKey.u16_0to50)
+    tile_ok = TestKey.u8_colorize(TestKey.u16_0to50)
     test_keys = {
-        'ranges': TestKey.fro0to50[np.newaxis],
+        'ranges': TestKey.fro(0, 50)[np.newaxis],
         'colors': TestKey.white[np.newaxis],
         'shape': tiles_in[0].shape
     }
@@ -125,11 +133,35 @@ def test_tile_1channel_gray():
     # START TEST
     test_id = '1channel_green_50to100'
     tiles_in = TestKey.u16_all[np.newaxis]
-    tile_ok = TestKey.to_bgr(TestKey.u16_50to100, TestKey.green)
+    tile_ok = TestKey.u8_colorize(TestKey.u16_50to100, TestKey.green)
     test_keys = {
-        'ranges': TestKey.fro50to100[np.newaxis],
+        'ranges': TestKey.fro(50, 100)[np.newaxis],
         'colors': TestKey.green[np.newaxis],
         'shape': tiles_in[0].shape
     }
     generic_test_tile(test_id, test_keys, tiles_in, tile_ok)
     del (test_id, test_keys, tiles_in, tile_ok)
+
+
+def test_tile_2channel_chess():
+    """ 2 channel cut and color
+    """
+    full_ranges = np.stack((TestKey.fro(0,100),)*2)
+    by_colors = np.stack((TestKey.blue, TestKey.yellow))
+    # START TEST
+    test_id = '2channel_chess'
+    tiles_in = [
+        TestKey.u16_chess0,
+        TestKey.u16_chess1,
+    ]
+    colors = by_colors
+    ranges = full_ranges
+    tile_ok = TestKey.u8_cut_norm_mean(tiles_in, ranges, colors)
+    test_keys = {
+        'ranges': ranges,
+        'colors': colors,
+        'shape': tiles_in[0].shape
+    }
+    # Combine 3 striped images in varied colors
+    generic_test_tile(test_id, test_keys, tiles_in, tile_ok)
+    del (test_id, test_keys, tiles_in, tile_ok, colors, ranges)
--- tests/constants.py
+++ tests/constants.py
@@ -8,8 +8,56 @@ import os
 class Key(object):
     """ Helper functions for unit tests
     """
+    def fro(*args):
+        """Make vector from percentages
+        """
+        if len(args) == 1:
+            args = args[0]
+        return np.float32(args)/100
 
-    def norm_cut(img, r):
+    def to_f32(img):
+        """Scale the dynamic range to 0.0 - 1.0
+
+        Arguments:
+            img: an integer image
+        """
+        try:
+            n_values = np.iinfo(img.dtype).max + 1
+            return np.float32(img / n_values)
+        except ValueError:
+            return img
+
+    def to_u8(f_img):
+        """Compress the range to 0 - 256
+
+        Arguments:
+            f_img: a uint32 image
+        """
+        return np.uint8(256*f_img)
+
+    def u8_cut_norm_mean(img_list, r_list, c_list):
+        """ Cut, normalize, color, and combine images
+
+        Arguments:
+            img_list: list of images to process
+            r_list: all min and max used to cut
+            r_list: all colors used to color
+
+        Returns:
+            uint8 cut, normalized average image
+        """
+        # Clip channels in range
+        f32_list = list(map(Key.to_f32, img_list))
+        step_0 = zip(f32_list, r_list)
+        norm_list = [Key.cut_norm(*s) for s in step_0]
+        # Map channels to colors
+        step_1 = zip(norm_list, c_list)
+        color_list = [Key.colorize(*s) for s in step_1]
+        # Return the mean of the colors
+        mean_image = np.mean(color_list, 0)
+        return Key.to_u8(mean_image)
+
+    def cut_norm(img, r):
         """ Cut and normalize image to range
 
         Arguments:
@@ -17,36 +65,41 @@ class Key(object):
             r: the min and max used to cut
 
         Returns:
-            uint8 cut and normalized image
+            float32 cut and normalized image
         """
-        f_img = Key.to_float(img)
+        f_img = Key.to_f32(img)
         cut_img = f_img * (f_img >= r[0]) * (f_img < r[1])
-        norm_img = (cut_img - r[0]) / np.diff(r)
-        return np.uint8(256*norm_img)
+        return (cut_img - r[0]) / np.diff(r)
 
-    def to_float(img):
-        """Scale the dynamic range to 0.0 - 1.0
-
-        Arguments:
-            img: an integer image
+    def u8_cut_norm(img, r):
+        """ Like cut_norm but returns uint8
         """
-        n_values = np.iinfo(img.dtype).max + 1
-        return np.float32(img / n_values)
+        return Key.to_u8(Key.cut_norm(img, r))
 
-    def to_bgr(img, color=[1, 1, 1]):
+    def colorize(img, color=[1, 1, 1]):
         """ Reshape into a color image
 
         Arguments:
             img: integer image to colorize
+
+        Returns:
+            float32 3-channel color image
         """
-        f_img = Key.to_float(img)
+        f_img = Key.to_f32(img)
         # Give the image a color dimension
         f_vol = f_img[:, :, np.newaxis]
-        f_bgr = np.repeat(f_vol, 3, 2) * color
-        return np.uint8(256*f_bgr)
+        return np.repeat(f_vol, 3, 2) * color
+
+    def u8_colorize(img, color=[1,1,1]):
+        """ Like colorize but returns uint8
+        """
+        return Key.to_u8(Key.colorize(img, color))
 
     def square(vec):
         """ Reshape a vector into an square image
+
+        Arguments:
+            vec: integer vector
         """
         sq_shape = np.ceil(np.sqrt((vec.shape[0],)*2))
         return np.reshape(vec, sq_shape.astype(vec.dtype))
