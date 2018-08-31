95e584f6e4e3258bd0c40ab0dbc22c8e0cce26e8
John Hoffer <john@hoff.in>
2018-08-31 14:25:15 -0400

    basic application

---
+++ application.py
@@ -0,0 +1,23 @@
+import tornado.web
+import tornado.wsgi
+import tornado.ioloop
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+webApp = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+# Wrapping the Tornado Application into a WSGI interface
+# As per AWS EB requirements, the WSGI interface must be named
+# 'application' only
+application = tornado.wsgi.WSGIAdapter(webApp)
+
+if __name__ == '__main__':
+    # If testing the server locally, start on the specific port
+    webApp.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
